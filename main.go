42894159d783d2c340da29e7d9efdbd6b1248739
Fredrick Oluoch <menty44@gmail.com>
2025-02-26 11:42:51 +0300

    Created the product struct

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// Definition of the product sctruct
+type Product struct {
+	ID         int
+	Name       string
+	Price      float64
+	Created    string
+	SalesCount int
+	ViewsCount int
+}
+
 func main() {
 	fmt.Println("Scnip task")
 }

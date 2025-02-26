5923384e6d9788607db7b898eb6452c8d8f0bd17
Fredrick Oluoch <menty44@gmail.com>
2025-02-26 11:51:17 +0300

    Looped product slices

--- main.go
+++ main.go
@@ -14,4 +14,15 @@ type Product struct {
 
 func main() {
 	fmt.Println("Scnip task")
+
+	// Create sample data
+	products := []Product{
+		{ID: 1, Name: "Alabaster Table", Price: 12.99, Created: "2019-01-04", SalesCount: 32, ViewsCount: 730},
+		{ID: 2, Name: "Zebra Table", Price: 44.49, Created: "2012-01-04", SalesCount: 301, ViewsCount: 3279},
+		{ID: 3, Name: "Coffee Table", Price: 10.00, Created: "2014-05-28", SalesCount: 1048, ViewsCount: 20123},
+	}
+
+	for _, p := range products {
+		fmt.Printf("ID: %d, Name: %s, Price: %.2f\n", p.ID, p.Name, p.Price)
+	}
 }
